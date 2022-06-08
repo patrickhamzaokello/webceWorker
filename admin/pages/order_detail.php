37c2e9da9978ec39b55e720a8a1cc9c04dccd2f4
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-06-08 19:00:47 +0300

    changed pho  in deliver

--- admin/pages/order_detail.php
+++ admin/pages/order_detail.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
-    <link rel="icon" type="image/x-icon" href="assets/z_favicon.png">
-
-    <link rel="stylesheet" href="../css/main.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <title>Order Detail</title>
-</head>
 <?php
 require("../config.php");
 $db = new Database();
@@ -26,6 +12,21 @@ require("../queries/classes/Cases.php");
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
+    <link rel="icon" type="image/x-icon" href="assets/z_favicon.png">
+
+    <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <title>Order Detail</title>
+</head>
+
 
 <body>
     <header>
